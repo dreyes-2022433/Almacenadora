c0731e7c482b868b0aa83435b3ac46b55770ab3e
Diego Reyes <dreyes-2022433@kinal.edu.gt>
2025-05-03 17:37:49 -0600

    Se agregan rutas para product

--- src/Products/product.routes.js
+++ src/Products/product.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express"
-import { addProduct, editProduct, deleteProduct, searchProducts } from "./product.controller"
+import { addProduct, editProduct, deleteProduct, searchProducts } from "./product.controller.js"
 
 const api = Router()
 
-api.post("/products", addProduct) // Agregar un nuevo producto
+api.post("/", addProduct) // Agregar un nuevo producto
 api.put("/products/:id", editProduct) // Editar un producto existente
 api.delete("/products/:id", deleteProduct) // Eliminar un producto
 api.get("/products", searchProducts) // Buscar y filtrar productos
+
+export default api
