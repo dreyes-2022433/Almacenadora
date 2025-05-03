661d3a422590e4e9fe9077e920fbe5bdd6af432e
Diego Reyes <dreyes-2022433@kinal.edu.gt>
2025-05-03 17:41:42 -0600

    feat: se agregan funcionalidades para supplier

--- src/Supplier/supplier.controller.js
+++ src/Supplier/supplier.controller.js
@@ -23,25 +23,5 @@ export const getSuppliers = async (req, res) => {
     }
 };
 
-// Registrar un nuevo cliente
-export const addClient = async (req, res) => {
-    try {
-        const { name, contact, company } = req.body;
-        const newClient = new Client({ name, contact, company });
-        await newClient.save();
-        res.status(201).json({ message: 'Cliente registrado exitosamente', client: newClient });
-    } catch (error) {
-        res.status(500).json({ message: 'Error al registrar el cliente', error });
-    }
-};
 
-// Obtener todos los clientes
-export const getClients = async (req, res) => {
-    try {
-        const clients = await Client.find();
-        res.status(200).json({ message: 'Clientes obtenidos exitosamente', clients });
-    } catch (error) {
-        res.status(500).json({ message: 'Error al obtener los clientes', error });
-    }
-};
 
