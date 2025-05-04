ec230031b6728bf065fc2959b74aeabd1824e2b7
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-05-03 23:46:12 -0600

    Se modifica la funcion de agregar producto

--- src/Products/product.controller.js
+++ src/Products/product.controller.js
@@ -4,49 +4,54 @@ import Supplier from '../Supplier/supplier.model.js'
 // Agregar un nuevo producto
 export const addProduct = async (req, res) => {
     try {
-        const { name, category, stock, supplier, entryDate,expirationDate, unitPrice } = req.body
+        const {
+            name,
+            category,
+            stock,
+            supplier,
+            entryDate,
+            expirationDate,
+            unitPrice
+        } = req.body
 
         const existSupplier = await Supplier.findById(supplier)
-
-        if(!existSupplier){
-            return res.status(404).send(
-                {
-                    success: false,
-                    message: 'Supplier not found'
-                }
-            )
+        if (!existSupplier) {
+            return res.status(404).send({
+                success: false,
+                message: 'Supplier not found'
+            })
         }
 
-        const newProduct = new Product(
-            { 
-                name, 
-                category, 
-                stock, 
-                supplier, 
-                entryDate, 
-                expirationDate,
-                unitPrice 
-            }
-        )
+        const newProduct = new Product({
+            name,
+            category,
+            stock,
+            supplier,
+            entryDate,
+            expirationDate,
+            unitPrice
+        })
 
         await newProduct.save()
-        return res.status(201).send(
-            { 
-                success: true,
-                message: 'Producto agregado exitosamente', 
-                product: newProduct 
-            }
-        )
+
+        await Supplier.findByIdAndUpdate(supplier, {
+            $addToSet: { products: newProduct._id } 
+        })
+
+        return res.status(201).send({
+            success: true,
+            message: 'Producto agregado exitosamente',
+            product: newProduct
+        })
     } catch (error) {
-        return res.status(500).send(
-            { 
-                success: false,
-                message: 'Error al agregar el producto', 
-                error 
-            }
-        )
+        return res.status(500).send({
+            success: false,
+            message: 'Error al agregar el producto',
+            error
+        })
     }
 }
+
 
 // Editar un producto existente
 export const editProduct = async (req, res) => {
