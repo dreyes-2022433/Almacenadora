05161e4382f4ae29d55094653b43c35b967b3668
jhernandez-2020477 <jhernandez-2020477@kinal.edu.gt>
2025-05-03 19:47:38 -0600

    Se agregan validaciones a la ruta de salida como entrada de productos

--- src/Entry/entry.routes.js
+++ src/Entry/entry.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import { registerEntry,registerExit, getEntrys } from "./entry.controller.js"
+import { validateRegisEntry, validateRegisExit } from "../../helpers/validators.js"
 
 const api = Router()
 
-api.post("/entry", registerEntry) // Registrar una entrada de producto
-api.post("/exit", registerExit) // Registrar una salida de producto
+api.post("/entry",[validateRegisEntry], registerEntry) // Registrar una entrada de producto
+api.post("/exit",[validateRegisExit], registerExit) // Registrar una salida de producto
 api.get("/movements", getEntrys) // Obtener todos los movimientos de productos
 
 export default api
