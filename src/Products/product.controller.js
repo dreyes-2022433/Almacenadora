123ceeda0a6358387e6cc9a6ce847b562cce6767
dhernandez-2020416 <dhernandez-2020416@kinal.edu.gt>
2025-05-03 21:46:54 -0600

    Se arregla estructura de código y se añaden validaciones a agregar, actualizar y eliminar producto

--- src/Products/product.controller.js
+++ src/Products/product.controller.js
@@ -1,58 +1,168 @@
-import Product from './product.model.js';
+import Product from './product.model.js'
+import Supplier from '../Supplier/supplier.model.js'
 
 // Agregar un nuevo producto
 export const addProduct = async (req, res) => {
     try {
         const { name, category, stock, supplier, entryDate,expirationDate, unitPrice } = req.body
-        const newProduct = new Product({ name, category, stock, supplier, entryDate, expirationDate,unitPrice });
-        await newProduct.save();
-        res.status(201).json({ message: 'Producto agregado exitosamente', product: newProduct });
+
+        const existSupplier = await Supplier.findById(supplier)
+
+        if(!existSupplier){
+            return res.status(404).send(
+                {
+                    success: false,
+                    message: 'Supplier not found'
+                }
+            )
+        }
+
+        const newProduct = new Product(
+            { 
+                name, 
+                category, 
+                stock, 
+                supplier, 
+                entryDate, 
+                expirationDate,
+                unitPrice 
+            }
+        )
+
+        await newProduct.save()
+        return res.status(201).send(
+            { 
+                success: true,
+                message: 'Producto agregado exitosamente', 
+                product: newProduct 
+            }
+        )
     } catch (error) {
-        res.status(500).json({ message: 'Error al agregar el producto', error });
+        return res.status(500).send(
+            { 
+                success: false,
+                message: 'Error al agregar el producto', 
+                error 
+            }
+        )
     }
-};
+}
 
 // Editar un producto existente
 export const editProduct = async (req, res) => {
     try {
-        const { id } = req.params;
-        const updates = req.body;
-        const updatedProduct = await Product.findByIdAndUpdate(id, updates, { new: true });
+        const { id } = req.params
+        const updates = req.body
+
+        const updatedProduct = await Product.findByIdAndUpdate(id, updates, { new: true })
+
         if (!updatedProduct) {
-            return res.status(404).json({ message: 'Producto no encontrado' });
+            return res.status(404).send(
+                { 
+                    success: false,
+                    message: 'Producto no encontrado' 
+                }
+            )
         }
-        res.status(200).json({ message: 'Producto actualizado exitosamente', product: updatedProduct });
+
+        if(updates.supplier){
+            const existSupplier = await Supplier.findById(updates.supplier)
+
+            if(!existSupplier){
+                return res.status(404).send(
+                    {
+                        success: false,
+                        message: 'Supplier not found'
+                    }
+                )
+            }    
+        }
+
+        return res.status(200).send(
+            { 
+                success: true,
+                message: 'Producto actualizado exitosamente', 
+                product: updatedProduct 
+            }
+        )
     } catch (error) {
-        res.status(500).json({ message: 'Error al actualizar el producto', error });
+        return res.status(500).send(
+            { 
+                success: false,
+                message: 'Error al actualizar el producto', 
+                error 
+            }
+        )
     }
-};
+}
 
 // Eliminar un producto
 export const deleteProduct = async (req, res) => {
     try {
-        const { id } = req.params;
-        const deletedProduct = await Product.findByIdAndDelete(id);
-        if (!deletedProduct) {
-            return res.status(404).json({ message: 'Producto no encontrado' });
+        const { id, reason } = req.body
+
+        if(!reason){
+            return res.status(400).send(
+                {
+                    success: false,
+                    message: 'No puedes eliminar un producto sin una razon'
+                }
+            )
         }
-        res.status(200).json({ message: 'Producto eliminado exitosamente', product: deletedProduct });
+
+        const deletedProduct = await Product.findByIdAndDelete(id)
+
+        if (!deletedProduct) {
+            return res.status(404).send(
+                { 
+                    success: false,
+                    message: 'Producto no encontrado' 
+                }
+            )
+        }
+        return res.status(200).send(
+            { 
+                success: true,
+                message: 'Producto eliminado exitosamente', 
+                product: deletedProduct 
+            }
+        )
     } catch (error) {
-        res.status(500).json({ message: 'Error al eliminar el producto', error });
+        return res.status(500).send(
+            { 
+                success: false,
+                message: 'Error al eliminar el producto', 
+                error 
+            }
+        )
     }
-};
+}
 
 // Buscar y filtrar productos
 export const searchProducts = async (req, res) => {
     try {
-        const { name, category, entryDate } = req.query;
-        const query = {};
-        if (name) query.name = { $regex: name, $options: 'i' }; // Búsqueda por nombre (insensible a mayúsculas)
-        if (category) query.category = category;
-        if (entryDate) query.entryDate = entryDate;
+        const { name, category, entryDate } = req.query
+        const query = {}
 
-        const products = await Product.find(query);
-        res.status(200).json({ message: 'Productos encontrados', products });
+        if (name) query.name = { $regex: name, $options: 'i' } // Búsqueda por nombre (insensible a mayúsculas)
+        if (category) query.category = category
+        if (entryDate) query.entryDate = entryDate
+
+        const products = await Product.find(query)
+        return res.status(200).send(
+            { 
+                success: true,
+                message: 'Productos encontrados', 
+                products 
+            }
+        )
     } catch (error) {
-        res.status(500).json({ message: 'Error al buscar productos', error });
+        return res.status(500).send(
+            { 
+                success: false,
+                message: 'Error al buscar productos', 
+                error 
+            }
+        )
     }
-};
+}
