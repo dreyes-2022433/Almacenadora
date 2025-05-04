c55e2d268bf94535f40fb6d07246d47728e7b8d9
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-05-03 23:49:22 -0600

    Se modifica la estructura del controlador de supplier

--- src/Supplier/supplier.controller.js
+++ src/Supplier/supplier.controller.js
@@ -1,27 +1,52 @@
-import Supplier from './supplier.model.js'; // Modelo de proveedores
-import Client from '../Client/client.model.js'; // Modelo de clientes
+import Supplier from './supplier.model.js'
+import Client from '../Client/client.model.js'
 
-// Registrar un nuevo proveedor
+
 export const addSupplier = async (req, res) => {
     try {
-        const { name, contact, productsSupplied } = req.body;
-        const newSupplier = new Supplier({ name, contact, productsSupplied });
-        await newSupplier.save();
-        res.status(201).json({ message: 'Proveedor registrado exitosamente', supplier: newSupplier });
-    } catch (error) {
-        res.status(500).json({ message: 'Error al registrar el proveedor', error });
-    }
-};
+        const { name, contact, productsSupplied } = req.body
+        const newSupplier = new Supplier(
+            { 
+                name, 
+                contact, 
+                productsSupplied 
+            }
+        )
+        await newSupplier.save()
+        res.status(201).send(
+            { 
+                message: 'Proveedor registrado exitosamente',
+                supplier: newSupplier 
+            }
+        )
 
-// Obtener todos los proveedores
+    } catch (error) {
+        res.status(500).send(
+            { 
+                message: 'Error al registrar el proveedor', 
+                error 
+            }
+        )
+    }
+}
+
 export const getSuppliers = async (req, res) => {
     try {
-        const suppliers = await Supplier.find();
-        res.status(200).json({ message: 'Proveedores obtenidos exitosamente', suppliers });
+        const suppliers = await Supplier.find().populate('products')
+
+        res.status(200).send({
+                message: 'Proveedores obtenidos exitosamente',
+                suppliers
+            }
+        )
     } catch (error) {
-        res.status(500).json({ message: 'Error al obtener los proveedores', error });
+            res.status(500).send({
+                message: 'Error al obtener los proveedores',
+                error
+            }
+        )
     }
-};
+}
 
 
 
