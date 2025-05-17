3e5349c670f3fad27d6bc2205c182475a06db3d0
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-05-16 22:37:31 -0600

    Se agregan la rutas de Client y Supplier

--- Frontend/src/routes.jsx
+++ Frontend/src/routes.jsx
@@ -2,10 +2,14 @@ import { AuthPage } from "./pages/AuthPage"
 import { NotFound } from "./pages/NotFound"
 
 import { Home } from "./pages/Mainpage.jsx"
+import { SupplierComponent } from "./components/Supplier/SupplierComponent.jsx"
+import { ClientComponent } from "./components/Cllient/ClientComponent.jsx"
 
 
 export const routes = [
     {path: '/', element: <AuthPage /> },
     {path: '/Home', element: <Home /> },
     {path: '/*', element: <NotFound /> },
+    {path: '/suppliers', element: <SupplierComponent/>},
+    {path: '/clients', element: <ClientComponent />},
 ]
