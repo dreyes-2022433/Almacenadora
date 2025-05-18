15e66f62b34dbd113d48fb0cb37dc68f9f94f98a
rsandoval-2020445 <rsandoval-2020445@kinal.edu.gt>
2025-05-17 23:21:47 -0600

    Mejorar NotificationComponent con nuevas funcionalidades o correcciones

--- Frontend/src/components/Notification/NotificationComponent.jsx
+++ Frontend/src/components/Notification/NotificationComponent.jsx
@@ -1,15 +1,14 @@
-//Diseño para las notificaciones
-import React from "react";
-import { Card,  CardBody , Text} from '@chakra-ui/react'
-export  const  NotificationComponent =() =>{
+import React from 'react'
+import { Card, CardBody, Text } from '@chakra-ui/react'
 
-return (
-<>
-
-</>
-    
-    )
-
-
-
+const NotificationComponent = ({ message, type }) => {
+  return (
+    <Card mb={6} bg="#D9D9D9" color="#284B63">
+      <CardBody>
+        <Text>{message}</Text>
+      </CardBody>
+    </Card>
+  )
 }
+
+export default NotificationComponent
