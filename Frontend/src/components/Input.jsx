15463eef6a95f80163b478397d94cd78e2087952
rsandoval-2020445 <rsandoval-2020445@kinal.edu.gt>
2025-05-17 23:21:39 -0600

    Mejorar el componente Input con estilos o funcionalidad mejorados

--- Frontend/src/components/Input.jsx
+++ Frontend/src/components/Input.jsx
@@ -1,30 +1,26 @@
-import React from "react"
+import React from 'react'
+import { FormControl, FormLabel, Input as ChakraInput } from '@chakra-ui/react'
 
+export const Input = ({ field, label, value, onChangeHandler, type, onBlurHandler }) => {
+  const handleValueChange = (e) => {
+    onChangeHandler(e.target.value, field)
+  }
 
+  const handleBlur = (e) => {
+    onBlurHandler(e.target.value, field)
+  }
 
-export const Input = ({ field,label,value,onChangeHandler, type,showErrorMessage,validationMessage,onBlurHandler}) => {
-    const handleValueChange = (e) => {
-        onChangeHandler(e.target.value , field)
-    }
-    const handleBlur = (e) => {
-        onBlurHandler(e.target.value, field)
-    }
-    return (
-        <>
-            <div className="input-label">
-                <span>{label}</span>
-            </div>
-        {(
-            <input 
-                type={type}
-                name={field}
-                value={value}
-                onChange={handleValueChange}
-                onBlur={handleBlur}
-                />
-        )
-        }
-        <span className='auth-form-validation-message' >{showErrorMessage && validationMessage}</span>
-        </>
-    )
+  return (
+    <FormControl>
+      <FormLabel htmlFor={field} className="auth-form-label">{label}</FormLabel>
+      <ChakraInput
+        id={field}
+        type={type}
+        name={field}
+        value={value}
+        onChange={handleValueChange}
+        onBlur={handleBlur}
+      />
+    </FormControl>
+  )
 }
