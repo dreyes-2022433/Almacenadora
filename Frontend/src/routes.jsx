155e46ea4b1e3f456a109bdba160f11533d3246b
dhernandez-2020416 <dhernandez-2020416@kinal.edu.gt>
2025-05-19 20:36:31 -0600

    Se agregan las funciones de entrys

--- Frontend/src/routes.jsx
+++ Frontend/src/routes.jsx
@@ -5,6 +5,8 @@ import { ReportsPage } from './pages/ReportsPage'
 import { Home } from "./pages/Mainpage.jsx"
 import { SupplierComponent } from "./components/Supplier/SupplierComponent.jsx"
 import { ClientComponent } from "./components/Cllient/ClientComponent.jsx"
+import { EntryComponent } from "./components/Entry/EntryComponnet.jsx"
+import { ExitComponent } from "./components/Entry/ExitComponent.jsx"
 
 
 export const routes = [
@@ -13,5 +15,7 @@ export const routes = [
     {path: '/*', element: <NotFound /> },
     {path: '/suppliers', element: <SupplierComponent/>},
     {path: '/clients', element: <ClientComponent />},
-    {path: '/reports', element: <ReportsPage /> }
+    {path: '/reports', element: <ReportsPage /> },
+    {path: '/entrys', element: <EntryComponent /> },
+    {path: '/exit', element: <ExitComponent /> }
 ]
